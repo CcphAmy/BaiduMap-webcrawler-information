e018ea16b67c6e381cd8ab651c5510675243c46a
陈沛杭 <wojiaopeihang@vip.qq.com>
2018-08-07 17:45:55 +0800

    代码更新
    代码更新

--- src/getBaiduMap.py
+++ src/getBaiduMap.py
@@ -33,22 +33,39 @@ class BaiduMap(object):
 
 	def getMapData(self,cityId,info_): 
 
-		qt        = "s"
-		rn        = "10"
-		modNum    = "10"
-		loopValue = 1
-
 		if cityId < 0 :
 			return -1
 
-		getUrl   = "http://api.map.baidu.com/?qt=" + qt + "&c=" + str(cityId) + "&wd=" + info_ + "&rn=" + rn + "&pn=1" + "&ie=utf-8&oue=1&fromproduct=jsapi&res=api&callback=BMap._rd._cbk7303&ak=E4805d16520de693a3fe707cdc962045";
-		webData  = requests.get(getUrl).text
-		# print(webData)
-		loopNum = re.search("\"total\":([\\s\\S]*?),",webData).group(1) #数量
-		reJson = re.search("content\":([\\s\\S]*?),\"current_city",webData).group(1)
-		print(loopNum)
-		jsonData = json.loads(reJson)
-		print(jsonData)
+		qt = "s" ; rn = "10" ; modNum = "10"
+		loopValue = 1 ; loopCount = 1
+
+		while loopValue <= loopCount:
+			getUrl    = "http://api.map.baidu.com/?qt=" + qt + "&c=" + str(cityId) + "&wd=" + info_ + "&rn=" + rn + "&pn=" + str(loopValue) + "&ie=utf-8&oue=1&fromproduct=jsapi&res=api&callback=BMap._rd._cbk7303&ak=E4805d16520de693a3fe707cdc962045";
+			webData   = requests.get(getUrl).text
+			
+			if loopValue == 1:
+				tempValue = int(re.search("\"total\":([\\s\\S]*?),",webData).group(1)) #数量
+				print(tempValue)
+				if tempValue >= 1:
+					loopCount = int(tempValue / 10)
+					if loopCount < tempValue / 10:
+						loopCount = loopCount + 1
+
+						###############################################
+						#            BUG，loopCount + 1 后报错        #
+						###############################################
+						
+						print("总共需要循环：" + str(loopCount))
+				else :
+					loopCount = 0
+
+			reJson   = re.search("content\":([\\s\\S]*?),\"current_city",webData).group(1)
+			jsonData = json.loads(reJson)
+			print(str(loopValue) + ":" ,end="")
+			print(jsonData)
+			loopValue = loopValue + 1
+
+
 
 if __name__ == '__main__':
 	obj = BaiduMap()
